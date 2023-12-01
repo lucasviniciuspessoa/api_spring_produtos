b3713ea5af90a8b6aa72909f1c5d29e3cf713fa3
Lucas Vinicius <lucasviniciuspessoaf@gmail.com>
2023-11-30 23:48:47 -0300

    add method register in ProductService

--- produtos/src/main/java/br/com/api/produtos/Services/ProductServices.java
+++ produtos/src/main/java/br/com/api/produtos/Services/ProductServices.java
@@ -1,10 +1,13 @@
 package br.com.api.produtos.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.produtos.Repository.ProductRepository;
 import br.com.api.produtos.model.ProductModel;
+import br.com.api.produtos.model.ResponseModel;
 
 @Service
 public class ProductServices {
@@ -12,8 +15,24 @@ public class ProductServices {
     @Autowired
     private ProductRepository pr;
 
+    @Autowired
+    private ResponseModel rm;
     public Iterable<ProductModel> list() {
         return pr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrar(ProductModel obj){
+         if(obj.getName().equals("")) {
+            rm.setMessage("O nome do produto é obrigatório");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+            
+         } else if(obj.getBrand().equals("") ) {
+            rm.setMessage("O nome da marca é obrigatório");
+            return new ResponseEntity<ResponseModel>(rm, HttpStatus.BAD_REQUEST);
+         } else {
+            return new ResponseEntity<ProductModel>(pr.save(obj), HttpStatus.CREATED);
+         }
+
     }
 
 }
--- produtos/src/main/java/br/com/api/produtos/controller/ProductController.java
+++ produtos/src/main/java/br/com/api/produtos/controller/ProductController.java
@@ -1,12 +1,22 @@
 package br.com.api.produtos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.Services.ProductServices;
+import br.com.api.produtos.model.ProductModel;
 
 @RestController
 
 public class ProductController {
     
+    @Autowired ProductServices ps;
+
+    @GetMapping("/list")
+    public Iterable<ProductModel>list() {
+        return ps.list();
+    }
 
     @GetMapping("/")
     public String rota() {
