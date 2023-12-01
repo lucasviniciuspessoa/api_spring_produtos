e1cf2c2643da9a6b1a818816202eaa3af0d93081
Lucas Vinicius <lucasviniciuspessoaf@gmail.com>
2023-11-30 23:30:21 -0300

    feat: create method list in productServices

--- produtos/src/main/java/br/com/api/produtos/Services/ProductServices.java
+++ produtos/src/main/java/br/com/api/produtos/Services/ProductServices.java
@@ -1,5 +1,19 @@
 package br.com.api.produtos.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.Repository.ProductRepository;
+import br.com.api.produtos.model.ProductModel;
+
+@Service
 public class ProductServices {
     
+    @Autowired
+    private ProductRepository pr;
+
+    public Iterable<ProductModel> list() {
+        return pr.findAll();
+    }
+
 }
